73738762b466ba2e027b0f7def837c9d7261335e
Roo2190 <133179590+Roo2190@users.noreply.github.com>
2025-11-09 15:13:23 +0700

    Update test-upload-model.py

--- test-upload-model.py
+++ test-upload-model.py
@@ -21,12 +21,13 @@ for model in models:
 uploaded_model = aiplatform.Model.upload(
     project="me-data-internal-sandbox",
     display_name=model_display_name,
-    artifact_uri=f"gs://abc-bkt",
+    artifact_uri=f"gs://yoshi-sandbox",
     serving_container_image_uri="asia-southeast2-docker.pkg.dev/me-data-internal-sandbox/custom-prediction-routine-sdk/test:latest",
     parent_model=parent_model.resource_name if parent_model else None,
     is_default_version=True,
     labels={
-        "version": "12"
+        "version": "13"
     },
     sync=True
+
 ) 
